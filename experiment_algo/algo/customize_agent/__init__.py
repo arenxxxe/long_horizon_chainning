02b0c8a58df2384db61a6391c17bbf7b3f3db0cf
ARENJESOTO <122794443+ARENJESOTO@users.noreply.github.com>
2024-07-01 18:52:13 +0800

    refactor files structure start

--- experiment_algo/algo/customize_agent/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# from ..viskill_agents.hier_agent import HierachicalAgent
-
-
-# def make_hier_agent(env_params, samplers, cfg):
-#     return HierachicalAgent(env_params, samplers, cfg)
--- experiment_algo/setup.py
+++
@@ -1,4 +0,0 @@
-
-from distutils.core import setup
-
-setup(name='longh', version='0.0.0', packages=['longh'])
